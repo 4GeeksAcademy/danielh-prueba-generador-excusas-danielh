fcbfe646f1a7178e6214b8baf9806b6c32a1ddc3
Daniel Hurtado Quero <131759030+OconnelDan@users.noreply.github.com>
2023-06-12 19:07:27 +0000

    trabajo terminado y funcionando

--- src/app.js
+++ src/app.js
@@ -7,5 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  const who = ["The dog", "My grandma", "His turtle", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "the keys", "the car"];
+  const when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  function generadorDeExcusas() {
+    let aleatorioWho = who[Math.floor(Math.random() * who.length)];
+    let aleatorioAction = action[Math.floor(Math.random() * action.length)];
+    let aleatorioWhat = what[Math.floor(Math.random() * what.length)];
+    let aleatorioWhen = when[Math.floor(Math.random() * when.length)];
+
+    let excusa = aleatorioWho.concat(
+      " ",
+      aleatorioAction,
+      " ",
+      aleatorioWhat,
+      " ",
+      aleatorioWhen
+    );
+
+    return excusa;
+  }
+  const excuse = document.querySelector("#excuse");
+  excuse.innerHTML = generadorDeExcusas();
   console.log("Hello Rigo from the console!");
+  console.log(generadorDeExcusas);
 };
